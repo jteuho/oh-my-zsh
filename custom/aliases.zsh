11d3e71fe5de07d7fd3af67cca7aa4dcfcba6cef
jteuho <juho.teuho@gofore.com>
2019-08-22 10:40:33 +0300

    ffg alias

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -12,6 +12,9 @@ alias -g CA="2>&1 | cat -A"
 alias -g NE="2> /dev/null"
 alias -g NUL="> /dev/null 2>&1"
 alias ff='find . -type f -name'
+ffg() {
+  find . -type f -name $1 -exec grep $2 '{}' ';' -print
+}
 
 alias gcim='git commit -m'
 alias gciam='git commit -am'
