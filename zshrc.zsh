f785c067ac602dcb490c0e472b98f03ab4742e5a
jteuho <juho.teuho@gofore.com>
2020-02-05 09:30:39 +0200

    Use lighter nvm-plugin

--- zshrc.zsh
+++ zshrc.zsh
@@ -49,7 +49,7 @@ HIST_STAMPS="yyyy-mm-dd"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=( git nvm )
+plugins=( git custom-nvm)
 
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
